2abdc6630a36218790f12edecfca9897a3167ff9
michanke <enangemmanueleta@gmail.com>
2023-02-17 15:37:05 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'c';
+	while
+		(a <= 'z') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
