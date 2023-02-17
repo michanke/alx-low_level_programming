b025d3894182e775c06f9ad0bfe02354b7a55158
michanke <enangemmanueleta@gmail.com>
2023-02-17 15:20:58 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
@@ -11,16 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
-	printf("%d is positive\n", n);
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+		printf("%d is zero\n", n);
 	else
-	{
-	printf("%d is negative\n", n);
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n, ld;
@@ -13,15 +14,10 @@ int main(void)
 	ld = n % 10;
 
 	if (ld > 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
-	}
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	else if (ld == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, ld);
-	}
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	else if (ld < 6 && ld != 0)
-	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
-	return (0);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	return (1);
 }
