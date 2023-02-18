2c5b5787db75056c761d29230c44d2c6280bee51
michanke <enangemmanueleta@gmail.com>
2023-02-17 16:19:45 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar((n % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
