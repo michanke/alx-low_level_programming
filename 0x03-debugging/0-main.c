87bf2244fc5e1ea012a9e16466ad15ed887c3fb7
michanke <enangemmanueleta@gmail.com>
2023-02-23 09:54:23 -0800

    0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,14 +4,12 @@
  * main - entry point
  * Return: 0
  */
-
 int main(void)
 {
-        int i;
+	int i;
 
-        i = 0;
-        positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
-
